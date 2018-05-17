14a30273c0c05f02f6893414298c5de6b36f409e
RelevantDanielChen <32559058+RelevantDanielChen@users.noreply.github.com>
2018-05-17 16:12:28 -0700

    Create Thomas.java

---
+++ Thomas.java
@@ -0,0 +1,19 @@
+/**
+* @summary This class defines the test player class, which we'll call Thomas because 
+* it's a random name that has absolutely no relevance to a person with the 
+* same name in real life. To be changed later if too weak/op. 
+**/
+public class Thomas extends Player
+{
+  /**
+  * Thomas gains 1000 health, 605 ms, and 0 wins by default.
+  **/
+  public static void main(String[] args)
+  {
+    health = 1000;
+    speed = 605;
+    wins = 0;
+  }
+  public 
+  
+}
