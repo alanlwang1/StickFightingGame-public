791b78be0cdaceeb18e3dd2d4a8dbc1785b6d913
RelevantDanielChen <32559058+RelevantDanielChen@users.noreply.github.com>
2018-05-18 13:21:18 -0700

    Create Obstacles.java

---
+++ Obstacles.java
@@ -0,0 +1,27 @@
+/**
+* Makes the obstacles that the players can put down. 
+* @author D. Chen
+* @version b1.0
+**/
+public abstract class Obstacles
+{
+  //instantiate needed variables
+  protected int health;
+  protected int length;
+  protected int width;
+  
+  /**
+  * Places an object down.
+  **/
+  public abstract void place();
+  
+  /**
+  * Recalculates health upon damage done
+  * @param damage the amount of damage the wall takes
+  **/
+  public void takeDamage(int damage)
+  {
+    dam
+  }
+  
+}
