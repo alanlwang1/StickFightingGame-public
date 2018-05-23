495efd5efe70a304ebf71ce4012280c771576166
Ryan Wei <ryanwei08@gmail.com>
2018-05-23 12:27:20 -0700

    animation sample

--- AnimationSample.java
+++ AnimationSample.java
@@ -37,9 +37,9 @@ public class AnimationSample extends Application {
                 final double height = 0.5 * primaryStage.getHeight();
                 final double radius = Math.sqrt(2) * Math.max(width, height);
                 for (int i=0; i<STAR_COUNT; i++) {
-                    final Node node = nodes[0];
-                    final double angle = angles[0];
-                    final long t = (now - start[0]) % 2000000000;
+                    final Node node = nodes[i];
+                    final double angle = angles[i];
+                    final long t = (now - start[i]) % 2000000000;
                     final double d = t * radius / 2000000000.0;
                     node.setTranslateX(Math.cos(angle) * d + width);
                     node.setTranslateY(Math.sin(angle) * d + height);
