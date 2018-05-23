83c264a4b7e8b4f45a39c8eccaeca18e098290cf
Ryan Wei <ryanwei08@gmail.com>
2018-05-23 13:05:38 -0700

    test

--- Runner.java
+++ Runner.java
@@ -6,7 +6,7 @@ import javafx.scene.image.*;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
+import java.util.concurrent.TimeUnit;
 /**
  * Hold down an arrow key to have your hero move around the screen.
  * Hold down the shift key to have the hero run.
@@ -16,7 +16,7 @@ public class Runner extends Application {
     private static final double W = 1800, H = 900;
 
     private static final String HERO_IMAGE_LOC =
-            "http://icons.iconarchive.com/icons/raindropmemory/legendora/64/Hero-icon.png";
+        "http://icons.iconarchive.com/icons/raindropmemory/legendora/64/Hero-icon.png";
 
     private Image heroImage;
     private Node  hero;
@@ -27,7 +27,7 @@ public class Runner extends Application {
     public void start(Stage stage) throws Exception {
         heroImage = new Image(HERO_IMAGE_LOC);
         hero = new ImageView(heroImage);
-        
+
         Group dungeon = new Group(hero);
 
         moveHeroTo(W / 2, H / 2);
@@ -35,76 +35,58 @@ public class Runner extends Application {
         Scene scene = new Scene(dungeon, 1800, 900, Color.FORESTGREEN);
 
         scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent event) {
-                switch (event.getCode()) {
-                    case UP:    goNorth = true; break;
-                    //case DOWN:  goSouth = true; break;
-                    case LEFT:  goWest  = true; break;
-                    case RIGHT: goEast  = true; break;
-                    case SHIFT: running = true; break;
+                @Override
+                public void handle(KeyEvent event) {
+                    switch (event.getCode()) {
+                        case UP: 
+                        for (int i = 0; i < 100; i++)
+                        {
+                            moveHeroBy(0,-1);
+                        }
+                        break;
+                        case DOWN: 
+                        for (int i = 0; i < 100; i++)
+                        {
+                            moveHeroBy(0,1);
+                        }
+                        break;
+                    }
                 }
-            }
-        });
-        
+            });
+
         scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent event) {
-                switch (event.getCode()) {
-                    case UP:    goNorth = false; break;
-                    //case DOWN:  goSouth = false; break;
-                    case LEFT:  goWest  = false; break;
-                    case RIGHT: goEast  = false; break;
-                    case SHIFT: running = false; break;
+                @Override
+                public void handle(KeyEvent event) {
+                    switch (event.getCode()) {
+                        case UP: 
+                        
+                        
+                            moveHeroBy(0,1);
+                        
+                        break;
+                    }
                 }
-            }
-        });
-        
+            });
+
         stage.setScene(scene);
         stage.show();
         
         AnimationTimer timer = new AnimationTimer() {
-            @Override
-            public void handle(long now) {
-                int dx = 0, dy = 0;
-
-                if (goEast)  dx += 1;
-                if (goWest)  dx -= 1;
-                if (goNorth) dy -= 1;
-                if (!(goNorth)) dy += 1;
-                if (running) { dx *= 3; dy *= 3; }
-                dx += 10;
-                dx -= 10;
-                moveHeroBy(dx, dy);
-            }
-        };
+                @Override
+                public void handle(long now) {
+                    int dy = 0;
+                    int dx = 0;
+                    if (goNorth)
+                    {
+                        dy-=1;
+                        goNorth = false;
+                    }
+                    moveHeroBy(dx, dy);
+                }
+            };
         timer.start();
     }
 
-    private void moveHeroBy(int dx, int dy) {
-        if (dx == 0 && dy == 0) return;
-
-        final double cx = hero.getBoundsInLocal().getWidth()  / 2;
-        final double cy = hero.getBoundsInLocal().getHeight() / 2;
-
-        double x = cx + hero.getLayoutX() + dx;
-        double y = cy + hero.getLayoutY() + dy;
-
-        moveHeroTo(x, y);
-    }
-
-    private void moveHeroTo(double x, double y) {
-        final double cx = hero.getBoundsInLocal().getWidth()  / 2;
-        final double cy = hero.getBoundsInLocal().getHeight() / 2;
-
-        if (x - cx >= 0 &&
-            x + cx <= W &&
-            y - cy >= 0 &&
-            y + cy <= H) {
-            hero.relocate(x - cx, y - cy);
-        }
-    }
-
     public static void main(String[] args) { launch(args); }
-    
+
 }
