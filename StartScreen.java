a40c8ec5b822a163e41dda73a9f1bb420b53e02b
Daniel Chen <danielchenofficial@gmail.com>
2018-05-30 21:23:03 -0700

    Done with the starting screen for now wew

--- StartScreen.java
+++ StartScreen.java
@@ -19,6 +19,11 @@ import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.Background;
+import javafx.scene.layout.GridPane;
+import javafx.geometry.Insets;
+import javafx.scene.image.ImageView;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
 
 /**
  * TestGui for the game
@@ -31,58 +36,108 @@ public class StartScreen
     private Button playButton, closeButton, bestOfThree, bestOfFive;
     private MainStage mainStage;
     private Scene scene;
+    private GridPane grid;
+    private final static double MIN_TILE_SIZE = 5;
+    private final static double MAX_TILE_SIZE = Double.MAX_VALUE;
     public StartScreen(MainStage ms)
     {
         //Assign reference to mainStage
         mainStage = ms;
         
+        grid = new GridPane();
+        //grid.setPadding(new Insets(10,10,10,10));
+        grid.setGridLinesVisible(true);
+        
         //make the text box for the main screen (welcome to game...)
         Text start = new Text();
-        start.setFont(new Font(45));
-        start.setText("Welcome to the Game of Honorable Summoners");
+        start.setFont(new Font(90));
+        start.setText("Welcome to the Game");
         start.setFill(Color.RED);
         start.setStrokeWidth(1.5);
         start.setStroke(Color.BLACK);
         start.setTextOrigin(VPos.TOP);
+        start.setId("start");
+        grid.setConstraints(start, 1, 2);
+        grid.setHalignment(start, HPos.CENTER);
+        //grid.setValignment(start, VPos.CENTER);
+        
+        
+        Text startCont = new Text();
+        startCont.setFont(new Font(90));
+        startCont.setText("of Honorable Summoners");
+        startCont.setFill(Color.RED);
+        startCont.setStrokeWidth(1.5);
+        startCont.setStroke(Color.BLACK);
+        grid.setConstraints(startCont, 1, 3);
+        grid.setHalignment(start, HPos.CENTER);
+        
+        Text alignmentBox = new Text();
+        alignmentBox.setFont(new Font(45));
+        alignmentBox.setText("Align");
+
+        
+  
         
         //make the picture for the stick figure (might add some other art later)
+        /**
         Canvas canvas = new Canvas(1800, 900); 
         GraphicsContext gc = canvas.getGraphicsContext2D(); 
         Image image = new Image("stick figure.png"); 
-        gc.drawImage(image, 0, 0); 
+        gc.drawImage(image, 0, 0);
+        **/
+        Image image = new Image("stick figure.png");
+        ImageView stickFigure = new ImageView(image);
+        stickFigure.setFitHeight(300);
+        stickFigure.setFitWidth(300);
+        grid.setConstraints(stickFigure, 1,6);
+        
+        grid.setHalignment(stickFigure, HPos.CENTER);
         //Image img = new Image("stick figure.png");
         //ImageView.setImage(img);
         
+
         //hello Daniel
         playButton = new Button();
         playButton.setText("Play");
         playButton.setFont(new Font(45));
+        playButton.setMinSize(300, 100);
+        
+        //playButton.setMaxSize(MAX_TILE_SIZE, MAX_TILE_SIZE);
         playButton.setOnAction(e -> 
         {
-            CharacterSelect cs = new CharacterSelect(1, mainStage);
-            Scene characterSelect = cs.getCSScene();
-            ms.changeScene(characterSelect);   
+               
         });
+        grid.setConstraints(playButton, 1, 8);
         
         bestOfThree = new Button();
         bestOfThree.setText("Best of Three");
         bestOfThree.setFont(new Font(45));
+        bestOfThree.setMinSize(300, 100);
         bestOfThree.setOnAction(e ->
         {
             
         });
+        grid.setConstraints(bestOfThree, 1, 8);
+        grid.setHalignment(bestOfThree, HPos.CENTER);
         
         bestOfFive = new Button();
         bestOfFive.setText("Best of Five");
         bestOfFive.setFont(new Font(45));
+        bestOfFive.setMinSize(300, 100);
         bestOfFive.setOnAction(e ->
         {
         });
+        grid.setConstraints(bestOfFive, 1, 8);
+        grid.setHalignment(bestOfFive, HPos.RIGHT);
         
-        closeButton = new Button("Close Program");
+        closeButton = new Button("Quit");
         closeButton.setOnAction(e -> MainStage.closeProgram());
         closeButton.setFont(new Font(30));
+        closeButton.setMinSize(210,50);
+        grid.setConstraints(closeButton, 0, 0);
+       
         
+        /**
         Group root = new Group(); 
         root.getChildren().add(canvas); 
         root.getChildren().add(start);
@@ -91,18 +146,30 @@ public class StartScreen
         root.getChildren().add(bestOfThree);
         root.getChildren().add(bestOfFive);
         root.setId("pane"); //credit: https://stackoverflow.com/questions/9738146/javafx-how-to-set-scene-background-image?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+        **/
         //ObservableList list = root.getChildren();
+        
         //list.add(start);
         //list.add(canvas);
         
         
-        scene = new Scene(root, 1800, 900);
+        grid.setHgap(0);
+        grid.setVgap(0);
+        grid.getChildren().addAll(start, startCont, playButton, bestOfThree, bestOfFive, closeButton, stickFigure);
+        
+        grid.setId("pane");
        
+        
+        
+        scene = new Scene(grid, 1800, 900);
+       
+        
         
         //scene.getStylesheets().addAll(this.getClass().getResource("background.css").toExternalForm());
         
          //credit: https://www.youtube.com/watch?v=o-f-rryAHPw
         //format the stick figure to be centered by x, a bit up on y
+        /**
         start.layoutXProperty().bind(scene.widthProperty().subtract(start.prefWidth(-1)).divide(2));
         start.layoutYProperty().bind(scene.heightProperty().divide(2).subtract(200));
         
@@ -121,9 +188,10 @@ public class StartScreen
         //format bo5 button to be at the same y position of play but more towards the right
         bestOfFive.layoutXProperty().bind(scene.widthProperty().subtract(start.prefWidth(-1)).divide(2).add(580));
         bestOfFive.layoutYProperty().bind(scene.heightProperty().divide(2).add(200));
+        **/
         
         scene.getStylesheets().addAll(this.getClass().getResource("background.css").toExternalForm());
-      
+        
 
     }
     public Scene getScene()
