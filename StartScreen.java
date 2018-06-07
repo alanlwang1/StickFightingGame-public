96a3b518476f98a5e394cc6b0a644434b914c3e4
Daniel Chen <danielchenofficial@gmail.com>
2018-06-07 12:35:04 -0700

    finished StartScreen, deleted instructions (not needed)

--- StartScreen.java
+++ StartScreen.java
@@ -1,34 +1,19 @@
-import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
-import javafx.scene.text.*;
-import javafx.geometry.Pos;
-import javafx.scene.layout.StackPane;
-import javafx.collections.ObservableList;
-import javafx.geometry.VPos;
-import javafx.scene.image.ImageView;
-import javafx.scene.control.Button;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundSize;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.GridPane;
-import javafx.geometry.Insets;
+//import needed packages
 import javafx.geometry.HPos;
 import javafx.geometry.VPos;
-import javafx.scene.control.Hyperlink;
-
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
 /**
- * TestGui for the game
+ * StartScreen displays the UI the user will immediately run into when they start the application.
+ * The first scene displayed in MainStage.
  *
- * @author Alan Wang
- * @version 052118
+ * @author Alan Wang, Daniel Chen
  */
 public class StartScreen
 {
@@ -45,15 +30,11 @@ public class StartScreen
      */
     public StartScreen(MainStage ms)
     {
-        
-        
         //Assign reference to mainStage
         mainStage = ms;
         
         grid = new GridPane();
-        //grid.setPadding(new Insets(10,10,10,10));
-        grid.setGridLinesVisible(true);
-        
+    
         //make the text box for the main screen (welcome to game...)
         Text start = new Text();
         start.setFont(new Font(112));
@@ -63,206 +44,131 @@ public class StartScreen
         start.setStroke(Color.BLACK);
         start.setTextOrigin(VPos.TOP);
         start.setId("start");
+        //position textbox
         grid.setConstraints(start, 2, 2);
         grid.setHalignment(start, HPos.CENTER);
-        //grid.setValignment(start, VPos.CENTER);
         
-        
+        //make second text box for main screen (on row below above textbox)
         Text startCont = new Text();
         startCont.setFont(new Font(112));
         startCont.setText("of Honorable Summoners");
         startCont.setFill(Color.RED);
         startCont.setStrokeWidth(1.5);
         startCont.setStroke(Color.BLACK);
+        //position textbox
         grid.setConstraints(startCont, 2, 3);
         grid.setHalignment(start, HPos.CENTER);
         
+        //make invisible text box for formatting (keeps right side as large as left)
         Text alignmentBox = new Text();
         alignmentBox.setFont(new Font(90));
         alignmentBox.setText("Align");
+        //position textbox
         grid.setConstraints(alignmentBox,3, 2);
         alignmentBox.setId("transparent");
         
+        //makes invisible text box for formatting (keeps middle large enough to make
+        //left and right symmetrical)
         Text bigBox = new Text();
         bigBox.setFont(new Font(50));
         bigBox.setText("Bet you will not read this ever Ms.Dafoe cus you ain's in APCSx");
+        //position textbox
         bigBox.setId("transparent");
         grid.setConstraints(bigBox, 2, 10);
-  
         
-        //make the picture for the stick figure (might add some other art later)
-        /**
-        Canvas canvas = new Canvas(1800, 900); 
-        GraphicsContext gc = canvas.getGraphicsContext2D(); 
-        Image image = new Image("stick figure.png"); 
-        gc.drawImage(image, 0, 0);
-        **/
+        //display a sprite
         Image image = new Image("stick figure trans.png");
-        
         ImageView stickFigure = new ImageView(image);
+        //set dimensions of picture
         stickFigure.setFitHeight(300);
         stickFigure.setFitWidth(300);
+        //position picture
         grid.setConstraints(stickFigure, 2,6);
-        
         grid.setHalignment(stickFigure, HPos.CENTER);
         
-        
-        //Image img = new Image("stick figure.png");
-        //ImageView.setImage(img);
-        
-        /*
-        //hello Daniel
-        playButton = new Button();
-        playButton.setText("Play");
-        playButton.setFont(new Font(45));
-        playButton.setMinSize(300, 100);
-        
-        //playButton.setMaxSize(MAX_TILE_SIZE, MAX_TILE_SIZE);
-        playButton.setOnAction(e -> 
-        {
-            
-        });
-        */
-        //grid.setConstraints(playButton, 1, 8);
-        
+        //makes a button for playing a best of three series of games
         bestOfThree = new Button();
         bestOfThree.setText("Play Best of Three");
         bestOfThree.setFont(new Font(45));
+        //format button
         bestOfThree.setMinSize(400, 100);
+        //move to Character Select when button is clicked
         bestOfThree.setOnAction(e ->
         {
             CharacterSelect cs = new CharacterSelect(1, mainStage);
             Scene csScene = cs.getCSScene(); 
             mainStage.changeScene(csScene);
         });
+        //position button
         grid.setConstraints(bestOfThree, 2, 8);
         grid.setHalignment(bestOfThree, HPos.LEFT);
         
+        //makes a button for playing a best of five series of games
         bestOfFive = new Button();
         bestOfFive.setText("Play Best of Five");
         bestOfFive.setFont(new Font(45));
+        //format button
         bestOfFive.setMinSize(400, 100);
+        //move to Character Select when button is clicked
         bestOfFive.setOnAction(e ->
         {
             CharacterSelect cs = new CharacterSelect(2, mainStage);
             Scene csScene = cs.getCSScene(); 
             mainStage.changeScene(csScene);
         });
+        //position button
         grid.setConstraints(bestOfFive, 2, 8);
         grid.setHalignment(bestOfFive, HPos.RIGHT);
         
+        //makes a button for quitting the game
         closeButton = new Button("Quit");
+        //request to close the game when clicked
         closeButton.setOnAction(e -> MainStage.closeProgram());
         closeButton.setFont(new Font(30));
+        //format the button
         closeButton.setMinSize(210,50);
+        //position button
         grid.setConstraints(closeButton, 0, 0);
-       
         
-        
-        
+        //make a button displaying instructions in external window
         final Button instructions = new Button("How to Play");
+        //really bad solution to calling a method in MainStage, but it works.
+        //will call .showInstructions(), which will display an external html page (in user's browser)
+        //with the instructions on it. 
         instructions.setOnAction(e -> 
         {
             MainStage z = new MainStage();
             z.showInstructions();
         });
         instructions.setFont(new Font(45));
+        //format button
         instructions.setMinSize(210,50);
-        
-      
-        
+        //position button
         grid.setConstraints(instructions, 2, 8);
         grid.setHalignment(instructions, HPos.CENTER);
         
-        
-        /**
-        Group root = new Group(); 
-        root.getChildren().add(canvas); 
-        root.getChildren().add(start);
-        root.getChildren().add(playButton);
-        root.getChildren().add(closeButton);
-        root.getChildren().add(bestOfThree);
-        root.getChildren().add(bestOfFive);
-        root.setId("pane"); //credit: https://stackoverflow.com/questions/9738146/javafx-how-to-set-scene-background-image?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
-        **/
-        //ObservableList list = root.getChildren();
-        
-        //list.add(start);
-        //list.add(canvas);
-        
-        
+        //define gridpane spacing (none to make it easier for me to see)
         grid.setHgap(0);
         grid.setVgap(0);
+        //add all nodes to the pane
         grid.getChildren().addAll(start, startCont, bestOfThree, bestOfFive, closeButton, stickFigure, alignmentBox, instructions, bigBox);
-        
+        //allow css to display background on pane
         grid.setId("pane");
        
-        
-        
+        //instantiate the scene
         scene = new Scene(grid, 1800, 900);
        
-        
-        
-        //scene.getStylesheets().addAll(this.getClass().getResource("background.css").toExternalForm());
-        
-         //credit: https://www.youtube.com/watch?v=o-f-rryAHPw
-        //format the stick figure to be centered by x, a bit up on y
-        /**
-        start.layoutXProperty().bind(scene.widthProperty().subtract(start.prefWidth(-1)).divide(2));
-        start.layoutYProperty().bind(scene.heightProperty().divide(2).subtract(200));
-        
-        //format the picture to be centered by x, maybe down on y
-        canvas.layoutXProperty().bind(scene.widthProperty().subtract(800));
-        canvas.layoutYProperty().bind(scene.heightProperty().divide(2).subtract(10));
-        
-        //format the first play button to be centered by x, at the bottom of the screen below the screen
-        playButton.layoutXProperty().bind(scene.widthProperty().subtract(start.prefWidth(-1)).divide(2).add(100));
-        playButton.layoutYProperty().bind(scene.heightProperty().divide(2).add(200));
-        
-        //format bo3 button to be at the same y position of play but more towards the center
-        bestOfThree.layoutXProperty().bind(scene.widthProperty().subtract(start.prefWidth(-1)).divide(2).add(250));
-        bestOfThree.layoutYProperty().bind(scene.heightProperty().divide(2).add(200));
-        
-        //format bo5 button to be at the same y position of play but more towards the right
-        bestOfFive.layoutXProperty().bind(scene.widthProperty().subtract(start.prefWidth(-1)).divide(2).add(580));
-        bestOfFive.layoutYProperty().bind(scene.heightProperty().divide(2).add(200));
-        **/
-        
+        //get css formatting from file 
         scene.getStylesheets().addAll(this.getClass().getResource("background.css").toExternalForm());
         
 
     }
+    /**
+     * Returns the scene from this class. 
+     * @return the scene displayed in this class
+     */
     public Scene getScene()
     {
         return scene;
     }
-
-    /**
-     * Credit: https://stackoverflow.com/questions/32781362/centering-an-image-in-an-imageview?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
-     
-    public void centerImage() {
-        Image img = imageView.getImage();
-        if (img != null) {
-            double w = 0;
-            double h = 0;
-
-            double ratioX = imageView.getFitWidth() / img.getWidth();
-            double ratioY = imageView.getFitHeight() / img.getHeight();
-
-            double reducCoeff = 0;
-            if(ratioX >= ratioY) {
-                reducCoeff = ratioY;
-            } else {
-                reducCoeff = ratioX;
-            }
-
-            w = img.getWidth() * reducCoeff;
-            h = img.getHeight() * reducCoeff;
-
-            imageView.setX((imageView.getFitWidth() - w) / 2);
-            imageView.setY((imageView.getFitHeight() - h) / 2);
-
-        }
-    }
-    */
 }
